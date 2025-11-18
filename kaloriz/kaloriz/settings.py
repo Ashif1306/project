bbd5c879e40b0517734bc4ce22b2cf2ef491a6f4
Ashif1306 <devashif.md@gmail.com>
2025-11-18 19:21:46 +0800

    update

--- kaloriz/kaloriz/settings.py
+++ kaloriz/kaloriz/settings.py
@@ -28,13 +28,17 @@ SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")  # Allow all hosts for development/testing
+ALLOWED_HOSTS = os.getenv(
+    "ALLOWED_HOSTS",
+    "localhost,127.0.0.1,muhashif.pythonanywhere.com"
+).split(",")
 
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:8000",
     "http://127.0.0.1:3000",
     "http://localhost:3000",
+    "https://muhashif.pythonanywhere.com",
 ]
 
 
--- kaloriz/kaloriz/urls.py
+++ kaloriz/kaloriz/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("catalog.urls")),
@@ -11,5 +12,6 @@ urlpatterns = [
     path("payment/", include("payment.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# SELALU tambahkan static & media (tidak pakai if settings.DEBUG)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
